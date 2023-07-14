30e35a7eab4030458c0c7d79162bad25e7e0b038
Павел Полищук <stiksxdi@yandex.ru>
2023-07-14 18:26:43 +0300

    Исправления

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,12 +1,13 @@
 from django.core.exceptions import BadRequest
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
+from rest_framework import serializers
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.fields import SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField
-from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, ReadOnlyField
-from rest_framework import status
+
 
 from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
 from users.models import Subscribe, User
