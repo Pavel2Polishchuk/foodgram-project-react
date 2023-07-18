17069e754f1877cdbbcbb3c097eec8524d8af343
Павел Полищук <stiksxdi@yandex.ru>
2023-07-18 20:33:02 +0300

    Исправление ошибок

--- backend/users/views.py
+++ backend/users/views.py
@@ -37,7 +37,7 @@ class CustomUserViewSet(UserViewSet):
             serializer.is_valid(raise_exception=True)
             Subscribe.objects.create(user=user, author=author)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED) 
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     @action(
         detail=False,
