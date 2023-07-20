6eb6b8298bd1e14418b319194cce2ae9d077cf07
Павел Полищук <stiksxdi@yandex.ru>
2023-07-20 16:26:08 +0300

    Правки

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,3 @@
-from django.core.exceptions import BadRequest
-from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers, status
--- backend/api/views.py
+++ backend/api/views.py
@@ -151,30 +151,31 @@ class RecipeViewSet(ModelViewSet):
         if not user.shopping_cart.exists():
             return Response(status=HTTP_400_BAD_REQUEST)
         return data_generation()
-        
+
+
 def data_generation(request):
-        user = request.user
-        ingredients = IngredientInRecipe.objects.filter(
-            recipe__shopping_cart__user=request.user
-        ).values(
-            'ingredient__name',
-            'ingredient__measurement_unit'
-        ).annotate(amount=Sum('amount'))
-        today = datetime.today()
-        shopping_list = (
-            f'Список покупок для: {user.get_full_name()}\n\n'
-            f'Дата: {today:%Y-%m-%d}\n\n'
-        )
-        shopping_list += '\n'.join([
-            f'- {ingredient["ingredient__name"]} '
-            f'({ingredient["ingredient__measurement_unit"]})'
-            f' - {ingredient["amount"]}'
-            for ingredient in ingredients
-        ])
-        shopping_list += f'\n\nFoodgram ({today:%Y})'
-        filename = f'{user.username}_shopping_list.txt'
-        response = HttpResponse(
-            shopping_list, content_type='text.txt; charset=utf-8'
-        )
-        response['Content-Disposition'] = f'attachment; filename={filename}'
-        return response
+    user = request.user
+    ingredients = IngredientInRecipe.objects.filter(
+        recipe__shopping_cart__user=request.user
+    ).values(
+        'ingredient__name',
+        'ingredient__measurement_unit'
+    ).annotate(amount=Sum('amount'))
+    today = datetime.today()
+    shopping_list = (
+        f'Список покупок для: {user.get_full_name()}\n\n'
+        f'Дата: {today:%Y-%m-%d}\n\n'
+    )
+    shopping_list += '\n'.join([
+        f'- {ingredient["ingredient__name"]} '
+        f'({ingredient["ingredient__measurement_unit"]})'
+        f' - {ingredient["amount"]}'
+        for ingredient in ingredients
+    ])
+    shopping_list += f'\n\nFoodgram ({today:%Y})'
+    filename = f'{user.username}_shopping_list.txt'
+    response = HttpResponse(
+        shopping_list, content_type='text.txt; charset=utf-8'
+    )
+    response['Content-Disposition'] = f'attachment; filename={filename}'
+    return response
